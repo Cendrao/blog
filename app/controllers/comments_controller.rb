1fd8bcc28ee1976300e65c5ef226b84891ac70ba
Matheus Cendrão <matheuscendrao@gmail.com>
2016-08-22 18:57:41 -0300

    added destroy comments and comments list in the page that shows post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,21 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
 
-    if @post.comments.create(comments_params)
+    if @post.comments.create(comment_params)
       redirect_to @post,
         notice: 'Comment was successfully created.'
     else
       redirect_to @post,
         alert: 'Error creating commit.'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
